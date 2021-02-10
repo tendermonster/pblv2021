64f0299dd01b132ab864856ff567cd99fbcbbd5d
tendermonster <artiom.blinovas@gmail.com>
2021-02-10 16:53:21 +0100

    bugfix and conf time per bin

--- blockTxInfos.py
+++ blockTxInfos.py
@@ -1,20 +1,16 @@
 from blockchainData import BlockchainData
 class BlockInfos:
-    __txHistBinLabels = []
-    __txSelectedFee = []
-    __txSelectedSize = []
-    __feesPerBinPerByte = []
-    __feesPerBinPerByteDict = []
     __averageTxSize = {}
     __averageTxSizeProc = {}
     __averagePpbSize = {}
+    __avgTimeToConf = {}
     def __init__(self,binWidth):
-        data = BlockchainData()
+        self.__data = BlockchainData()
         self.__binWidth = binWidth
-        self.__txs = data.txs
-        self.__sizes = data.sizes
-        self.__fees = data.fees
-        self.__init_data(binWidth)
+        self.__txs = self.__data.txs
+        self.__sizes = self.__data.sizes
+        self.__fees = self.__data.fees
+        self.__init_data()
         self.__statistics()
     def __getHistBinHeight(self,data,binWidth):
         dataDict = {i:data[i] for i in range(0,len(data))}
@@ -52,88 +48,192 @@ class BlockInfos:
         while val >= binWidth*inc:
             inc = inc + 1
         return inc
-    def __init_data(self,binWidth):
-        self.__txHistBinLabels = [self.__getHistBinHeight(i,self.__binWidth) for i in self.__sizes]
-        #[dataset][0 bin width,1 indexes]
-        #block mit transaktionen
-        self.__txSelectedFee = []
-        for block in range(0,len(self.__txHistBinLabels)):
+    #0 - size 1 - fee 2-time
+    def __getTxSelectedBin(self,binLabels,t):
+        txSelectedFee = []
+        for block in range(0,len(binLabels)):
             binCluster = []
             #alle bins rausholen
-            for binIndexes in list(self.__txHistBinLabels[block][1].values()):
+            for binIndexes in list(binLabels[block][1].values()):
                 #aus jeden bin indizes rausholen
                 binGroup = []
                 for index in binIndexes:
-                    binGroup.append(self.__txs[block][index].fee)
+                    if t == 0:
+                        binGroup.append(self.__txs[block][index].size)
+                    elif t==1:
+                        binGroup.append(self.__txs[block][index].fee)
+                    elif t==2:
+                        binGroup.append(self.__txs[block][index].time)
                 binCluster.append(binGroup)
-            self.__txSelectedFee.append(binCluster)
-
-        #[block][Bin] = values
-        #block mit transaktionen
-        self.__txSelectedSize = []
-        for block in range(0,len(self.__txHistBinLabels)):
-            binCluster = []
-            #alle bins rausholen
-            for binIndexes in list(self.__txHistBinLabels[block][1].values()):
-                #aus jeden bin indizes rausholen
-                binGroup = []
-                for index in binIndexes:
-                    binGroup.append(self.__txs[block][index].size)
-                binCluster.append(binGroup)
-            self.__txSelectedSize.append(binCluster)
-        self.__feesPerBinPerByte = []
+            txSelectedFee.append(binCluster)
+        return txSelectedFee
+    def __getFeePerBinPerByte(self,txFee,txSizes):
+        feesPerBinPerByte = []
         #block
-        for i in range(0,len(self.__txSelectedFee)):
+        for i in range(0,len(txFee)):
             #bins
             bins = []
-            for j in range(0,len(self.__txSelectedFee[i])):
-                binValuesFee = self.__txSelectedFee[i][j]
-                binValuesSize = self.__txSelectedSize[i][j]
+            for j in range(0,len(txFee[i])):
+                binValuesFee = txFee[i][j]
+                binValuesSize = txSizes[i][j]
                 bins.append(sum(binValuesFee)/sum(binValuesSize))
-            #ppb.append(bins)
-            self.__feesPerBinPerByte.append(bins)
-        self.__feesPerBinPerByteDict = []
-        for i in range(0,len(self.__txHistBinLabels)):
-            binCat = dict(zip(list(self.__txHistBinLabels[i][0].keys()),self.__feesPerBinPerByte[i]))
-            self.__feesPerBinPerByteDict.append(binCat)
+            feesPerBinPerByte.append(bins)
+        return feesPerBinPerByte
+    def __computeAveragePpbSize(self,ppbDict):
         #average fee per byte in all bins
-
         #todo unite and get kn
-        ppbKeys = [list(txDict.keys()) for txDict in self.__feesPerBinPerByteDict]
+        ppbKeys = [list(txDict.keys()) for txDict in ppbDict]
         inc = 1
-        binCount = {binWidth*i:0 for i in range(1,401)}
-        binCountSum = {binWidth*i:0 for i in range(1,401)}
-        for dictInd in range(len(self.__feesPerBinPerByteDict)):
+        binCount = {self.__binWidth*i:0 for i in range(1,401)}
+        binCountSum = {self.__binWidth*i:0 for i in range(1,401)}
+        for dictInd in range(len(ppbDict)):
             for keyTx in ppbKeys[dictInd]:
                 if keyTx in binCount:
                     binCount[keyTx] += 1
-                    binCountSum[keyTx] += self.__feesPerBinPerByteDict[dictInd][keyTx]
+                    binCountSum[keyTx] += ppbDict[dictInd][keyTx]
                 else:
                     binCount[keyTx] = 1
-                    binCountSum[keyTx] = self.__feesPerBinPerByteDict[dictInd][keyTx]
-        self.__averagePpbSize = {}
+                    binCountSum[keyTx] = ppbDict[dictInd][keyTx]
+        averagePpbSize = {}
         for i in list(binCount.keys()):
             if binCount[i] != 0:
-                self.__averagePpbSize[i] = binCountSum[i]/binCount[i]
-        #calculating average transaction count in bin
-        #todo unite and get kn
-        txSizesKeys = [list(txDict[0].keys()) for txDict in self.__txHistBinLabels]
+                averagePpbSize[i] = binCountSum[i]/binCount[i]
+        return averagePpbSize
+    def __computeAverageTxSizePerBin(self,txHistBinLabels):
+        txSizesKeys = [list(txDict[0].keys()) for txDict in txHistBinLabels]
         inc = 1
-        binCount = {binWidth*i:0 for i in range(1,401)}
+        binCount = {self.__binWidth*i:0 for i in range(1,401)}
         #binCount[300] += 1
-        binCountSum = {binWidth*i:0 for i in range(1,401)}
-        for dictInd in range(len(self.__txHistBinLabels)):
+        binCountSum = {self.__binWidth*i:0 for i in range(1,401)}
+        for dictInd in range(len(txHistBinLabels)):
             for keyTx in txSizesKeys[dictInd]:
                 if keyTx in binCount:
                     binCount[keyTx] += 1
-                    binCountSum[keyTx] += self.__txHistBinLabels[dictInd][0][keyTx]
+                    binCountSum[keyTx] += txHistBinLabels[dictInd][0][keyTx]
                 else:
                     binCount[keyTx] = 1
-                    binCountSum[keyTx] = self.__txHistBinLabels[dictInd][0][keyTx]
-        self.__averageTxSize = {}
+                    binCountSum[keyTx] = txHistBinLabels[dictInd][0][keyTx]
+        averageTxSize = {}
         for i in list(binCount.keys()):
             if binCount[i] != 0:
-                self.__averageTxSize[i] = binCountSum[i]/binCount[i]
+                averageTxSize[i] = binCountSum[i]/binCount[i]
+        return averageTxSize
+    # def __computeAvgConfTimePerBin(self,txHistBinLabels):
+    #     txHistIndexes = txHistBinLabels[:][1]
+    #     blocks = self.__data.blocks
+    #     blockTimes = [block.time for block in blocks]
+    #     blockHeights =[block.height for block in blocks]
+    #     blockHeightTimeDict = dict(list(zip(blockHeights,blockTimes)))
+    #     confTimePerBin = []
+    #     for block in range(0,len(txHistBinLabels)):
+    #         binCluster = []
+    #         #alle bins rausholen
+    #         for binDict in txHistBinLabels[block][1]:
+    #             #aus jeden bin indizes rausholen
+    #             binGroup = {}
+    #             timesList = []
+    #             for index in txHistBinLabels[block][1][binDict]:
+    #                 tx = self.__txs[block][index]
+    #                 txBlock = tx.block_height
+    #                 confTime = blockHeightTimeDict[txBlock]-tx.time #zeit in s
+    #                 timesList.append(confTime)
+    #             binGroup[binDict] = timesList
+    #             binCluster.append(binGroup)
+    #         confTimePerBin.append(binCluster)
+    #     #get an average conf time per size
+    #
+    #     keys = [list(txDict.keys()) for txDict in txHistIndexes]
+    #     inc = 1
+    #     binCount = {self.__binWidth*i:0 for i in range(1,401)}
+    #     binCountSum = {self.__binWidth*i:0 for i in range(1,401)}
+    #     for dictInd in range(len(confTimePerBin)):
+    #         dictKeys = list(keys[dictInd])
+    #         for keyTx in range(0,len(dictKeys)):
+    #             if dictKeys[keyTx] in confTimePerBin:
+    #                 binCount[dictKeys[keyTx]] += 1
+    #                 print(confTimePerBin[dictInd][keyTx][dictKeys[keyTx]])
+    #                 binCountSum[dictKeys[keyTx]] += sum(confTimePerBin[dictInd][keyTx][dictKeys[keyTx]])
+    #             else:
+    #                 binCount[dictKeys[keyTx]] = 1
+    #                 binCountSum[dictKeys[keyTx]] += sum(confTimePerBin[dictInd][keyTx][dictKeys[keyTx]])
+    #
+    #     avgTimeToConf = {}
+    #     for i in list(binCount.keys()):
+    #         if binCount[i] != 0:
+    #             self.__avgTimeToConf[i] = binCountSum[i]/binCount[i]
+    #     return avgTimeToConf
+
+    def __computeAvgConfTimePerBin(self,txHistBinLabels):
+        txHistIndexes = [i[1] for i in txHistBinLabels]
+        blocks = self.__data.blocks
+        blockTimes = [block.time for block in blocks]
+        blockHeights =[block.height for block in blocks]
+        blockHeightTimeDict = dict(list(zip(blockHeights,blockTimes)))
+        confTimePerBin = []
+        for block in range(0,len(txHistIndexes)):
+            binCluster = []
+            #alle bins rausholen
+            for binDict in txHistIndexes[block]:
+                #aus jeden bin indizes rausholen
+                binGroup = {}
+                timesList = []
+                for index in txHistIndexes[block][binDict]:
+                    tx = self.__txs[block][index]
+                    txBlock = tx.block_height
+                    confTime = blockHeightTimeDict[txBlock]-tx.time #zeit in s
+                    timesList.append(confTime)
+                binGroup[binDict] = timesList
+                binCluster.append(binGroup)
+            confTimePerBin.append(binCluster)
+        #get an average conf time per size
+        #return confTimePerBin
+        keys = [list(txDict.keys()) for txDict in txHistIndexes]
+        inc = 1
+        binCount = {self.__binWidth*i:0 for i in range(1,401)}
+        binCountSum = {self.__binWidth*i:0 for i in range(1,401)}
+        for dictInd in range(0,len(confTimePerBin)):
+            dictKeys = list(keys[dictInd])
+            for keyTx in range(0,len(dictKeys)):
+                if dictKeys[keyTx] in binCount:
+                    binCount[dictKeys[keyTx]] += 1
+                    confTimes = confTimePerBin[dictInd][keyTx][dictKeys[keyTx]]
+                    binCountSum[dictKeys[keyTx]] += sum(confTimes)/len(confTimes)
+                else:
+                    binCount[dictKeys[keyTx]] = 1
+                    binCountSum[dictKeys[keyTx]] = 1
+                    confTimes = confTimePerBin[dictInd][keyTx][dictKeys[keyTx]]
+                    binCountSum[dictKeys[keyTx]] += sum(confTimes)/len(confTimes)
+
+        avgTimeToConf = {}
+        for i in list(binCount.keys()):
+            if binCount[i] != 0:
+                avgTimeToConf[i] = binCountSum[i]/binCount[i]
+        return avgTimeToConf
+
+    def __init_data(self):
+        txHistBinLabels = [self.__getHistBinHeight(i,self.__binWidth) for i in self.__sizes]
+        #[dataset][0 bin width,1 indexes]
+        #block mit transaktionen
+        txSelectedFee = self.__getTxSelectedBin(txHistBinLabels,1)
+        #[block][Bin] = values
+        #block mit transaktionen
+        txSelectedSize = self.__getTxSelectedBin(txHistBinLabels,0)
+
+        feesPerBinPerByte = self.__getFeePerBinPerByte(txSelectedFee,txSelectedSize)
+        #create dict with indexes as keys
+        feesPerBinPerByteDict = []
+        for i in range(0,len(txHistBinLabels)):
+            binCat = dict(zip(list(txHistBinLabels[i][0].keys()),feesPerBinPerByte[i]))
+            feesPerBinPerByteDict.append(binCat)
+
+        #calculating average transaction count in bin
+        self.__averagePpbSize = self.__computeAveragePpbSize(feesPerBinPerByteDict)
+
+        self.__averageTxSize = self.__computeAverageTxSizePerBin(txHistBinLabels)
+
+        self.__avgTimeToConf = self.__computeAvgConfTimePerBin(txHistBinLabels)
+
+        txSelectedSize = self.__getTxSelectedBin(txHistBinLabels,2)
     def __statistics(self):
         y_val = self.__averageTxSize.values()
         x_keys = self.__averageTxSize.keys()
@@ -145,6 +245,8 @@ class BlockInfos:
         return self.__averageTxSizeProc
     def getAvgPpbSize(self):
         return self.__averagePpbSize
+    def getAverageTimeToConf(self):
+        return self.__avgTimeToConf
     #die methode liefert zum einen daten einer histogram und zum andern
     #die indizen von den datenpunkten die sich in den bins befinden damit man dieser wieder zuordnen kann
     # rückgabe [binGrößen,indizen von Daten die in den jeweiligen bins drin sind]
