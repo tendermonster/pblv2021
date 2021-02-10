ef1d0106e61ae645405e84a76412e6d001dce37f
tendermonster <artiom.blinovas@gmail.com>
2021-02-10 22:29:08 +0100

    index page and basic layout for other pages

--- flaskweb/app/routes.py
+++ flaskweb/app/routes.py
@@ -10,14 +10,22 @@ def index():
     title="Home"
     return render_template('index.html',**locals())
 
-@app.route('/form')
-def myform():
-    return render_template('my-form.html')
+@app.route('/track')
+def track():
+    test1 = Test(123)
+    title="Home"
+    return render_template('track.html',**locals())
 
-@app.route('/form', methods=['POST'])
+@app.route('/stats')
+def stats():
+    test1 = Test(123)
+    title="Home"
+    return render_template('stats.html',**locals())
+
+@app.route('/track', methods=['POST'])
 def my_form_post():
     text = request.form['text']
     processed_text = text.upper()
     test1 = Test(12333333333333)
     title="Home"
-    return render_template('index.html',**locals())
+    return render_template('track.html',**locals())
