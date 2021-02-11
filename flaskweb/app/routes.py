00510ee15b57d96ba91f5d963d542b3c1b8129f7
Marten Budelmann <budelman@hm.edu>
2021-02-11 14:53:50 +0100

    changed sites

--- flaskweb/app/routes.py
+++ flaskweb/app/routes.py
@@ -3,6 +3,7 @@ from app import app
 from testClass import Test
 import sys
 from blockTxInfos import BlockInfos
+from Mempool import Blockdifference, TransactionsMempool, TransactionsBlock, NewTransactions
 from testClass import Test
 data = BlockInfos(300)
 testData = Test(123)
@@ -13,12 +14,18 @@ def index():
 
 @app.route('/track')
 def track():
+    b = Blockdifference()
     test1 = Test(123)
-    title="Home"
+    title = b.tx_pro_block()	
     return render_template('track.html',**locals())
 
 @app.route('/stats')
 def stats():
+    block = Blockdifference()
+    tm = TransactionsMempool()
+    tb = TransactionsBlock()
+    nt = NewTransactions()
+    martendata = [int(round(tm.get_median(), 0)), int(round(tb.get_median(), 0)), round(block.get_avg() / 60, 2), int(round(nt.get_avg(), 0)), nt.get_10_cmp()]
     test1 = Test(123)
     title="Home"
     someData = data.getAvgPpbSize()
@@ -31,4 +38,4 @@ def my_form_post():
     processed_text = text.upper()
     test1 = Test(12333333333333)
     title="Home"
-    return render_template('track.html',**locals())
+    return render_template('track2.html',**locals())
